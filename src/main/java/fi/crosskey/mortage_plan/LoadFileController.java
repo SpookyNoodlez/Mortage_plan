a8b715d4679f298210121b5f147f0f0fbe3a832e
SpookyNoodlez <55265731+SpookyNoodlez@users.noreply.github.com>
2022-02-13 21:31:12 +0200

    Moved the stringToProspect method to its own class

--- src/main/java/fi/crosskey/mortage_plan/LoadFileController.java
+++ src/main/java/fi/crosskey/mortage_plan/LoadFileController.java
@@ -15,6 +15,7 @@ public class LoadFileController {
     @FXML
     protected void loadFile() {
         ArrayList<Prospect> prospects = new ArrayList<>();
+        StringToProspectConverter converter = new StringToProspectConverter();
 
         try {
             //Making sure the file is read using UTF_8 charset
@@ -28,7 +29,7 @@ public class LoadFileController {
             //read lines until reaching end of file
             while((currentLine = reader.readLine()) != null){
                 //Split the string up and put it in a Prospect record
-                prospects.add(stringToProspect(currentLine));
+                prospects.add(converter.stringToProspect(currentLine));
             }
             reader.close();
         } catch (IOException e) {
@@ -59,55 +60,5 @@ public class LoadFileController {
         }
         //Display result
         resultDisplay.setText(displayString);
-    }
-
-    private Prospect stringToProspect(String line){
-
-        final String quotedNameRegex = "(\"[^\"]+\")"; //Anything except quotes between quotes
-                                          //OR
-        final String unquotedNameRegex = "([^,]+)"; //Anything except commas
-
-        final String totalLoanRegex = "(\\d+\\.?\\d*)"; //One or more numbers followed by zero or one dots followed by any number of numbers
-        final String interestRegex = "(\\d\\d?\\.?\\d*)"; //One or two numbers followed by zero or one dots followed by any number of numbers
-        final String yearsRegex = "(\\d+)"; //One or more numbers
-
-        String completedRegex = "(" + quotedNameRegex + "|" + unquotedNameRegex  + ")" + ","
-                + totalLoanRegex + "," + interestRegex + "," + yearsRegex;
-
-        //If the line doesn't fit the format
-        if (!line.matches(completedRegex)){
-            return new Prospect("Bad data",-1,-1,-1,-1);
-        }
-
-        try{
-            //Split by commas, go backwards and combine the last ones
-            String[] tokens = line.split(",");
-
-            //The last tokens are always years followed by interest and the loan amount
-            int years = Integer.parseInt(tokens[tokens.length-1]);
-            float interest = Float.parseFloat(tokens[tokens.length-2]) / 100.f; //convert from percent to decimal
-            double totalLoan = Double.parseDouble(tokens[tokens.length-3]);
-
-            //All remaining tokens make up the name
-            String name = "";
-            for(int i = 0; i <= tokens.length-4; i++){
-                name += tokens[i];
-                //add back the comma
-                if(i != tokens.length-4){
-                    name += ",";
-                }
-            }
-            //Remove quotes from the name
-            name = name.replace("\"","");
-
-            LoanCalculator calculator = new LoanCalculator();
-            double monthlyPayment = calculator.calculateMonthlyPayment(totalLoan,interest,years);
-
-            return new Prospect(name, totalLoan, interest, years, monthlyPayment);
-        }
-        catch(NumberFormatException e){
-            //If letters go in the number fields return a bad prospect to show a load error in the list
-            return new Prospect("Bad data",-1,-1,-1,-1);
-        }
     }
 }
